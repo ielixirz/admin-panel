13c3f1d47121395d558e664418bc4d3b8a9d3aeb
itays123 <itays2005@gmail.com>
2020-10-13 16:40:15 +0300

    REFACTOR [DRY]: get, set, delete draft functions

--- src/services/drafts.ts
+++ src/services/drafts.ts
@@ -1,5 +1,17 @@
 import { api, getCallData } from './api'
 
+export function getDraft(contextType: string, contextId: string|null): Promise<any> {
+  return api.get('/api/drafts', { params: { contextType, contextId } }).then(getCallData);
+}
+
+export function setDraft(draftData: any) {
+  return api.put('/api/drafts', { data: draftData });
+}
+
+export function deleteDraft(contextData: string, contextId: string|null) {
+  return api.delete('/api/drafts', { params: { contextData, contextId } })
+}
+
 export function getDraftPost(postId: string|null): Promise<any> {
   return api.get('/api/drafts', { params: { contextType: 'post', postId } }).then(getCallData);
 }
